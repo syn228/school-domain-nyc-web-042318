cee05bcc817e60a0ad0d615c3d4e1c1c23782b30
Aaron Na <syn228@nyu.edu>
2018-04-25 13:33:30 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School
+  def initialize(roster)
+    @roster = {}
+  end
+  def grade(grade)
+    @grade = grade
+  end
+  def add_student(name, grade)
+    @name = [name]
+    @grade = grade
+    
+    @roster[grade] = []
+    @roster[grade] << name
+  end
+  def roster
+    @roster
+  end
+end
+
+
+
